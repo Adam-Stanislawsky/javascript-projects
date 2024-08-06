b72dabeae089fff59000cecf1f9c7ce280c93dbc
Adam Stanislawsky <adamstanislawsky@Adams-MBP.attlocal.net>
2024-08-05 20:30:50 -0500

    dom studio

--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -1,2 +1,33 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+window.addEventListener("load", function() {
+    let takeOffButton = document.getElementById("takeoff")
+    takeOffButton.addEventListener("click", function(event){
+        let answer = window.confirm("Confirm that the shuttle is ready for takeoff.");
+        if (answer) {
+            document.getElementById("flightStatus").innerHTML = "Shuttle in Flight";
+            document.getElementById("shuttleBackground").style.backgroundColor = "blue"
+            document.getElementById("spaceShuttleHeight").innerHTML = 10000;
+        }
+
+        let landButton = document.getElementById("landing");
+        landButton.addEventListener("click", function(event){
+            window.alert("The shuttle is landing, landing gear engaged.");
+            document.getElementById("flightStatus").innerHTML = "Shuttle has landed";
+            document.getElementById("shuttleBackground").style.backgroundColor = 'green';
+            document.getElementById("spaceShuttleHeight").innerHTML = 0;
+        })
+
+        let abortButton = document.getElementById("missionAbort");
+        abortButton.addEventListener("click", function(event){
+            let answer = window.confirm("Confirm that you want to abort the mission")
+            if (answer){
+                document.getElementById("flightStatus").innerHTML = "Mission Aborted";
+                document.getElementById("shuttleBackground").style.backgroundColor = 'green'
+                document.getElementById("spaceShuttleHeight").innerHTML = 0;
+            }
+        })
+    });
+
+});
